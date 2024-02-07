d2c1861d919f4d9156a5c42eacffa73db4895ef2
donovanainsley <donovan_weise@live.co.uk>
2024-02-07 19:55:19 +0000

    the heroku toolbelt

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import os
 import json
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py "):
+    import env
 
 app = Flask(__name__)
-
-
+app.secret_key = os.environ.get("SECRET_KEY")
 @app.route('/')
 def index():
     return render_template("index.html")
@@ -32,7 +32,8 @@ def about_member(member_name):
 @app.route('/contact', methods=["GET", "POST"])
 def contact():
     if request.method == "POST" :
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template('contact.html', page_title="Contact")
 
 
